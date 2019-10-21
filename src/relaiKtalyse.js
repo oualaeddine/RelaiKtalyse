2524edc06d821427f400028cc49be2f2df8b7c5e
Berrehal Ouala Eddine <berrehaloualaeddine@gmail.com>
2019-10-21 10:00:47 +0200

    Update relaiKtalyse.js

--- src/relaiKtalyse.js
+++ src/relaiKtalyse.js
@@ -1,8 +1,11 @@
+let chambre = "3";
+
+
 //const Configuration = require('./Configuration.js');
 const BeaconScanner = require('./BLEScanner.js');
 const WebsocketClient = require('./WebsocketClient.js');
 //configuration = new Configuration("./configuration.json");
-beaconScanner = new BeaconScanner("3");
+beaconScanner = new BeaconScanner(chambre);
 const websocketClient = new WebsocketClient("ws://79.137.78.86:8090");
 beaconScanner.addObserver(websocketClient);
 beaconScanner.startScan();
